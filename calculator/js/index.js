9898dc39d534b1eb4ba37eebcbe5d45927b2a072
Jeni Hoyt <ziahrahl@gmail.com>
2022-12-28 16:24:35 -0500

    write function to handle click on number buttons

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -5,13 +5,30 @@ let previousOperator;
 const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
+    console.log(value)
+    if (isNaN(value)) {
+        // this is not a number
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+}
 
+function handleSymbol(symbol) {}
+
+function handleNumber (numberString) {
+    if (buffer === '0') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
+    }
+    console.log(buffer)
+    screen.innerText = buffer
 }
 
 function init () {
     document.querySelector('.calc-buttons')
     .addEventListener('click', function(event) {
-        console.log(event.target.innerText);
         buttonClick(event.target.innerText);
     })
 }
