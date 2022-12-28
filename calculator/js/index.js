115a10749d172e0ddec6495c8ff4e54906fc8261
Jeni Hoyt <ziahrahl@gmail.com>
2022-12-28 16:52:47 -0500

    write function to handle Math

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -5,16 +5,30 @@ let previousOperator;
 const screen = document.querySelector('.screen');
 
 function buttonClick(value) {
-    console.log(value)
     if (isNaN(value)) {
         // this is not a number
         handleSymbol(value);
+        // this is a number
     } else {
         handleNumber(value);
     }
+    screen.innerText = buffer;
 }
 
-function handleSymbol(symbol) {}
+function handleSymbol(symbol) {
+    switch (value) {
+        case 'C': 
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '&#43;': // addition
+        case '&#8722;': // subtraction 
+        case '&#247;': // multiply
+        case '&#215;': // divide
+            handleMath(symbol);
+            break;
+    }
+}
 
 function handleNumber (numberString) {
     if (buffer === '0') {
@@ -22,8 +36,29 @@ function handleNumber (numberString) {
     } else {
         buffer += numberString;
     }
-    console.log(buffer)
-    screen.innerText = buffer
+}
+
+function handleMath (symbol) {
+    if (buffer === '0') {
+        // do nothing
+        return;
+    }
+
+    const intBuffer = parseInt(buffer);
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
+        flushOperation(intBuffer);
+    }
+
+    previousOperator = symbol;
+
+    buffer = '0';
+}
+
+function flushOperation(intBuffer) {
+
 }
 
 function init () {
