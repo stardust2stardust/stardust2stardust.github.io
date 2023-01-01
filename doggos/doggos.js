b70fc1ee5a6e5a630f7bcb397c9c5b791ef2cb64
Jeni Hoyt <ziahrahl@gmail.com>
2023-01-01 12:52:58 -0500

    add button to get more dog pictures

--- doggos/doggos.js
+++ doggos/doggos.js
@@ -1,19 +1,25 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-const promise = fetch(BREEDS_URL);
-
-promise
-  .then(function(response) {
-    const processingPromise = response.json();
+function addDoggo () {
+    const promise = fetch(BREEDS_URL);
     
-    return processingPromise;
-  })
-  .then(function(processedResponse) {
-    const img = document.createElement('img');
-    img.src = processedResponse.message;
-    img.alt = "cute doggo";
+    promise
+      .then(function(response) {
+        const processingPromise = response.json();
+        
+        return processingPromise;
+      })
+      .then(function(processedResponse) {
+        const img = document.createElement('img');
+        img.src = processedResponse.message;
+        img.alt = "cute doggo";
+    
+        document.querySelector(".doggos").appendChild(img);
+      });
 
-    document.querySelector(".doggos").appendChild(img);
-  });
+}
+
+document.querySelector('.add-doggo').addEventListener('click', addDoggo)
+
 
 console.log("this will log first");
