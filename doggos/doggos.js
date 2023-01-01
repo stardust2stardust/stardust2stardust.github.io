3752df0d6f7e706aafa8700188beae22f0d380b9
Jeni Hoyt <ziahrahl@gmail.com>
2023-01-01 12:49:44 -0500

    use fetch to get random picture and insert into document

--- doggos/doggos.js
+++ doggos/doggos.js
@@ -1,14 +1,19 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-const promise = fetch(DOG_URL);
+const promise = fetch(BREEDS_URL);
 
 promise
   .then(function(response) {
     const processingPromise = response.json();
+    
     return processingPromise;
   })
   .then(function(processedResponse) {
-    console.log(breeds);
+    const img = document.createElement('img');
+    img.src = processedResponse.message;
+    img.alt = "cute doggo";
+
+    document.querySelector(".doggos").appendChild(img);
   });
 
 console.log("this will log first");
