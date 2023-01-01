96445b80a7e98872d03e28f5badfe225733f4c92
Jeni Hoyt <ziahrahl@gmail.com>
2023-01-01 12:22:31 -0500

    animate emoji in nav using popmotion

--- gallery2/my-gallery.js
+++ gallery2/my-gallery.js
@@ -1,11 +1,3 @@
-// new Swiper(".swiper", {
-//     speed: 400,
-//     spaceBetween: 100,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev"
-//     }
-// })
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -31,4 +23,29 @@ const swiper = new Swiper('.swiper', {
     // add effect
     effect: "flip",
 
+  });
+
+  // motion
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.brand');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      // mass: 1,
+      // damping: 10
+    }).start(ballXY);
   });
